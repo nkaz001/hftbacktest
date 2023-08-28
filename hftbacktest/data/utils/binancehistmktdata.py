2b3137c3c643e9a96621e8fb0c3cd46ab0922dde
nkaz001 <nkaz001@protonmail.com>
2023-08-28 10:21:19 -0400

    Update #27. An optional header argument has been added to determine whether the first row is a header or not.

--- hftbacktest/data/utils/binancehistmktdata.py
+++ hftbacktest/data/utils/binancehistmktdata.py
@@ -1,3 +1,4 @@
+import csv
 from typing import Optional, Literal
 
 import numpy as np
@@ -10,7 +11,8 @@ from ... import DEPTH_EVENT, TRADE_EVENT, DEPTH_SNAPSHOT_EVENT
 def convert_snapshot(
         snapshot_filename: str,
         output_filename: Optional[str] = None,
-        feed_latency: float = 0
+        feed_latency: float = 0,
+        has_header: Optional[bool] = None,
 ) -> NDArray:
     r"""
     Converts Binance Historical Market Data files into a format compatible with HftBacktest.
@@ -24,6 +26,7 @@ def convert_snapshot(
         snapshot_filename: Snapshot filename
         output_filename: If provided, the converted data will be saved to the specified filename in ``npz`` format.
         feed_latency: Artificial feed latency value to be added to the exchange timestamp to create local timestamp.
+        has_header: True if the given file has a header, it will automatically detect it if set to None.
 
     Returns:
         Converted data compatible with HftBacktest.
@@ -31,22 +34,53 @@ def convert_snapshot(
     ss_bid = []
     ss_ask = []
 
+    timestamp_col = None
+    side_col = None
+    price_col = None
+    qty_col = None
+
     # Reads snapshot file
     print('Reading %s' % snapshot_filename)
-    with open(snapshot_filename, 'r') as f:
-        # Skips the header
-        f.readline()
-        while True:
-            line = f.readline()
-            if line is None or line == '':
-                break
-            cols = line.strip().split(',')
+    with open(snapshot_filename, 'r', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if timestamp_col is None:
+                if has_header is None:
+                    if row[0] == 'symbol':
+                        has_header = True
+                    else:
+                        has_header = False
 
-            exch_timestamp = int(cols[1])
+                if has_header:
+                    header = row
+                else:
+                    header = [
+                        'symbol',
+                        'timestamp',
+                        'trans_id',
+                        'first_update_id',
+                        'last_update_id',
+                        'side',
+                        'update_type',
+                        'price',
+                        'qty'
+                    ]
+                    if len(header) != len(row):
+                        raise ValueError
+
+                timestamp_col = header.index('timestamp')
+                side_col = header.index('side')
+                price_col = header.index('price')
+                qty_col = header.index('qty')
+
+                if has_header:
+                    continue
+
+            exch_timestamp = int(row[timestamp_col])
             loc_timestamp = exch_timestamp + feed_latency
-            side = 1 if cols[4] == 'b' else -1
-            price = float(cols[6])
-            qty = float(cols[7])
+            side = 1 if row[side_col] == 'b' else -1
+            price = float(row[price_col])
+            qty = float(row[qty_col])
 
             if side == 1:
                 ss_bid.append([
@@ -86,7 +120,9 @@ def convert(
         buffer_size: int = 100_000_000,
         feed_latency: float = 0,
         base_latency: float = 0,
-        method: Literal['separate', 'adjust'] = 'separate'
+        method: Literal['separate', 'adjust'] = 'separate',
+        depth_has_header: Optional[bool] = None,
+        trades_has_header: Optional[bool] = None
 ) -> NDArray:
     r"""
     Converts Binance Historical Market Data files into a format compatible with HftBacktest.
@@ -105,6 +141,8 @@ def convert(
         base_latency: The value to be added to the feed latency.
                       See :func:`.correct_local_timestamp`.
         method: The method to correct reversed exchange timestamp events. See :func:`..validation.correct`.
+        depth_has_header: True if the given file has a header, it will automatically detect it if set to None.
+        trades_has_header: True if the given file has a header, it will automatically detect it if set to None.
 
     Returns:
         Converted data compatible with HftBacktest.
@@ -112,21 +150,52 @@ def convert(
     tmp_depth = np.empty((buffer_size, 6), np.float64)
     row_num = 0
 
-    print('Reading %s' % depth_filename)
-    with open(depth_filename, 'r') as f:
-        # Skips the header
-        f.readline()
-        while True:
-            line = f.readline()
-            if line is None or line == '':
-                break
-            cols = line.strip().split(',')
+    timestamp_col = None
+    side_col = None
+    price_col = None
+    qty_col = None
 
-            exch_timestamp = int(cols[1])
+    print('Reading %s' % depth_filename)
+    with open(depth_filename, 'r', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if timestamp_col is None:
+                if depth_has_header is None:
+                    if row[0] == 'symbol':
+                        depth_has_header = True
+                    else:
+                        depth_has_header = False
+
+                if depth_has_header:
+                    header = row
+                else:
+                    header = [
+                        'symbol',
+                        'timestamp',
+                        'trans_id',
+                        'first_update_id',
+                        'last_update_id',
+                        'side',
+                        'update_type',
+                        'price',
+                        'qty'
+                    ]
+                    if len(header) != len(row):
+                        raise ValueError
+
+                timestamp_col = header.index('timestamp')
+                side_col = header.index('side')
+                price_col = header.index('price')
+                qty_col = header.index('qty')
+
+                if depth_has_header:
+                    continue
+
+            exch_timestamp = int(row[timestamp_col])
             loc_timestamp = exch_timestamp + feed_latency
-            side = 1 if cols[4] == 'b' else -1
-            price = float(cols[6])
-            qty = float(cols[7])
+            side = 1 if row[side_col] == 'b' else -1
+            price = float(row[price_col])
+            qty = float(row[qty_col])
 
             # Insert DEPTH_EVENT
             tmp_depth[row_num] = [
@@ -143,22 +212,49 @@ def convert(
     tmp_trades = np.empty((buffer_size, 6), np.float64)
     row_num = 0
 
-    print('Reading %s' % trades_filename)
-    with open(trades_filename, 'r') as f:
-        while True:
-            line = f.readline()
-            if line is None or line == '':
-                break
-            cols = line.strip().split(',')
-            # Checks if it's a header.
-            if cols[0] == 'id':
-                continue
+    timestamp_col = None
+    side_col = None
+    price_col = None
+    qty_col = None
 
-            exch_timestamp = int(cols[4])
+    print('Reading %s' % trades_filename)
+    with open(trades_filename, 'r', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if timestamp_col is None:
+                if trades_has_header is None:
+                    if row[0] == 'id':
+                        trades_has_header = True
+                    else:
+                        trades_has_header = False
+
+                if trades_has_header:
+                    header = row
+                else:
+                    header = [
+                        'id',
+                        'price',
+                        'qty',
+                        'quote_qty',
+                        'time',
+                        'is_buyer_maker'
+                    ]
+                    if len(header) != len(row):
+                        raise ValueError
+
+                timestamp_col = header.index('time')
+                side_col = header.index('is_buyer_maker')
+                price_col = header.index('price')
+                qty_col = header.index('qty')
+
+                if trades_has_header:
+                    continue
+
+            exch_timestamp = int(row[timestamp_col])
             loc_timestamp = exch_timestamp + feed_latency
-            side = -1 if cols[5] else 1  # trade initiator's side
-            price = float(cols[1])
-            qty = float(cols[2])
+            side = -1 if row[side_col] else 1  # trade initiator's side
+            price = float(row[price_col])
+            qty = float(row[qty_col])
 
             # Insert TRADE_EVENT
             tmp_trades[row_num] = [
