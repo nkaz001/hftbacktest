93d052b415bfdc5fd014bc88ae0ff1318fced417
ToughStyle <35249976+ToughStyle@users.noreply.github.com>
2023-10-25 11:57:58 +0800

    Update marketdepth.py
    Use order-book's SortedDict to replace @numba.jitclass & numba.typed.Dict, with performance improved a lot!

--- hftbacktest/marketdepth.py
+++ hftbacktest/marketdepth.py
@@ -1,10 +1,5 @@
 import sys
-
-from numba import njit, float64, int64
-from numba.experimental import jitclass
-from numba.typed import Dict
-from numba.types import DictType
-
+from order_book import SortedDict
 from .reader import COL_PRICE, COL_QTY, COL_SIDE
 from .order import BUY, SELL
 
@@ -12,65 +7,47 @@ INVALID_MIN = -sys.maxsize
 INVALID_MAX = sys.maxsize
 
 
-@njit
-def depth_below(depth, start, end):
-    for t in range(start - 1, end - 1, -1):
-        if t in depth and depth[t] > 0:
-            return t
-    return INVALID_MIN
-
-
-@njit
-def depth_above(depth, start, end):
-    for t in range(start + 1, end + 1):
-        if t in depth and depth[t] > 0:
-            return t
-    return INVALID_MAX
-
-
-@jitclass
 class MarketDepth:
-    tick_size: float64
-    lot_size: float64
-    ask_depth: DictType(int64, float64)
-    bid_depth: DictType(int64, float64)
-    best_bid_tick: int64
-    best_ask_tick: int64
-    low_bid_tick: int64
-    high_ask_tick: int64
-
     def __init__(self, tick_size, lot_size):
         self.tick_size = tick_size
         self.lot_size = lot_size
-        self.ask_depth = Dict.empty(int64, float64)
-        self.bid_depth = Dict.empty(int64, float64)
-        self.best_bid_tick = INVALID_MIN
-        self.best_ask_tick = INVALID_MAX
-        self.low_bid_tick = INVALID_MAX
-        self.high_ask_tick = INVALID_MIN
+        self.ask_depth = SortedDict(ordering='ASC')
+        self.bid_depth = SortedDict(ordering='DESC')        
 
     def apply_snapshot(self, data):
-        self.best_bid_tick = INVALID_MIN
-        self.best_ask_tick = INVALID_MAX
-        self.low_bid_tick = INVALID_MAX
-        self.high_ask_tick = INVALID_MIN
-        self.bid_depth.clear()
-        self.ask_depth.clear()
         for row in data:
             price_tick = round(row[COL_PRICE] / self.tick_size)
             qty = row[COL_QTY]
             if row[COL_SIDE] == BUY:
-                if price_tick > self.best_bid_tick:
-                    self.best_bid_tick = price_tick
-                if price_tick < self.low_bid_tick:
-                    self.low_bid_tick = price_tick
                 self.bid_depth[price_tick] = qty
             elif row[COL_SIDE] == SELL:
-                if price_tick < self.best_ask_tick:
-                    self.best_ask_tick = price_tick
-                if price_tick > self.high_ask_tick:
-                    self.high_ask_tick = price_tick
-                self.ask_depth[price_tick] = qty
+                self.ask_depth[price_tick] = qty    
+
+    @property
+    def best_bid_tick(self):
+        return self.get_bid_tick()    
+    
+    @property
+    def best_ask_tick(self):
+        return self.get_ask_tick()    
+    
+    @property
+    def low_bid_tick(self):
+        return self.get_bid_tick(-1)    
+
+    @property
+    def high_ask_tick(self):
+        return self.get_ask_tick(-1)    
+
+    def get_bid_tick(self, idx=0):
+        if len(self.bid_depth) > 0:
+            return self.bid_depth.index(idx)
+        return INVALID_MAX if idx==0 else INVALID_MIN
+    
+    def get_ask_tick(self, idx=0):
+        if len(self.ask_depth) > 0:
+            return self.ask_depth.index(idx)
+        return INVALID_MIN if idx==0 else INVALID_MAX
 
     def clear_depth(
             self,
@@ -79,28 +56,18 @@ class MarketDepth:
     ):
         clear_upto = round(clear_upto_price / self.tick_size)
         if side == BUY:
-            if self.best_bid_tick != INVALID_MIN:
+            if len(self.bid_depth) > 0:
                 for t in range(self.best_bid_tick, clear_upto - 1, -1):
                     if t in self.bid_depth:
                         del self.bid_depth[t]
-                self.best_bid_tick = depth_below(self.bid_depth, clear_upto - 1, self.low_bid_tick)
-                if self.best_bid_tick == INVALID_MIN:
-                    self.low_bid_tick = INVALID_MAX
         elif side == SELL:
-            if self.best_ask_tick != INVALID_MAX:
+            if len(self.ask_depth) > 0:
                 for t in range(self.best_ask_tick, clear_upto + 1):
                     if t in self.ask_depth:
                         del self.ask_depth[t]
-                self.best_ask_tick = depth_above(self.ask_depth, clear_upto + 1, self.high_ask_tick)
-                if self.best_ask_tick == INVALID_MAX:
-                    self.high_ask_tick = INVALID_MIN
         else:
-            self.bid_depth.clear()
-            self.ask_depth.clear()
-            self.best_bid_tick = INVALID_MIN
-            self.best_ask_tick = INVALID_MAX
-            self.low_bid_tick = INVALID_MAX
-            self.high_ask_tick = INVALID_MIN
+            self.ask_depth = SortedDict(ordering='ASC')
+            self.bid_depth = SortedDict(ordering='DESC')  
 
     def update_bid_depth(
             self,
@@ -118,10 +85,7 @@ class MarketDepth:
 
         if round(qty / self.lot_size) == 0:
             del self.bid_depth[price_tick]
-            if price_tick == self.best_bid_tick:
-                self.best_bid_tick = depth_below(self.bid_depth, self.best_bid_tick, self.low_bid_tick)
-                if self.best_bid_tick == INVALID_MIN:
-                    self.low_bid_tick = INVALID_MAX
+
         else:
             if price_tick > self.best_bid_tick:
                 if callback is not None:
@@ -129,9 +93,7 @@ class MarketDepth:
 
                 self.best_bid_tick = price_tick
                 if self.best_bid_tick >= self.best_ask_tick:
-                    self.best_ask_tick = depth_above(self.ask_depth, self.best_bid_tick, self.high_ask_tick)
-            if price_tick < self.low_bid_tick:
-                self.low_bid_tick = price_tick
+                    self.clear_depth(SELL, self.best_bid_tick * self.tick_size) 
 
     def update_ask_depth(
             self,
@@ -149,10 +111,7 @@ class MarketDepth:
 
         if round(qty / self.lot_size) == 0:
             del self.ask_depth[price_tick]
-            if price_tick == self.best_ask_tick:
-                self.best_ask_tick = depth_above(self.ask_depth, self.best_ask_tick, self.high_ask_tick)
-                if self.best_ask_tick == INVALID_MAX:
-                    self.high_ask_tick = INVALID_MIN
+
         else:
             if price_tick < self.best_ask_tick:
                 if callback is not None:
@@ -160,6 +119,4 @@ class MarketDepth:
 
                 self.best_ask_tick = price_tick
                 if self.best_ask_tick <= self.best_bid_tick:
-                    self.best_bid_tick = depth_below(self.bid_depth, self.best_ask_tick, self.low_bid_tick)
-            if price_tick > self.high_ask_tick:
-                self.high_ask_tick = price_tick
+                    self.clear_depth(BUY, self.best_ask_tick * self.tick_size)
