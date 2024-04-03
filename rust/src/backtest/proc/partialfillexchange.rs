f831f50bcc81a77fac145ce98fb98c0e363be624
nkaz001 <nkaz001@protonmail.com>
2024-04-03 11:14:27 -0400

    chore: prevent the execution of code under development.

--- rust/src/backtest/proc/partialfillexchange.rs
+++ rust/src/backtest/proc/partialfillexchange.rs
@@ -154,7 +154,7 @@ where
             if self.queue_model.is_filled(order, &self.depth) {
                 // fixme
                 // let q_qty = (-order.q[0] / self.depth.lot_size * self.depth.lot_size).ceil();
-                todo!()
+                todo!();
                 let q_qty = 0f32;
                 let exec_qty = q_qty.min(qty).min(order.leaves_qty);
                 self.filled_orders.push(order.order_id);
@@ -180,7 +180,7 @@ where
             if self.queue_model.is_filled(order, &self.depth) {
                 // fixme
                 // let q_qty = (-order.q[0] / self.depth.lot_size * self.depth.lot_size).ceil();
-                todo!()
+                todo!();
                 let q_qty = 0f32;
                 let exec_qty = q_qty.min(qty).min(order.leaves_qty);
                 self.filled_orders.push(order.order_id);
