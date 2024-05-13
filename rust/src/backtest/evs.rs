88b1b8d56bc074c49c5362f07dc80b81c48553dd
nkaz001 <nkaz001@protonmail.com>
2024-05-13 10:37:37 -0400

    perf: pack the event timestamp set for more efficient caching.

--- rust/src/backtest/evs.rs
+++ rust/src/backtest/evs.rs
@@ -1,3 +1,6 @@
+use std::mem;
+use std::mem::{forget, size_of};
+
 #[derive(Clone, Copy)]
 #[repr(C, align(32))]
 pub(crate) struct Event {
@@ -7,98 +10,116 @@ pub(crate) struct Event {
 }
 
 #[derive(Eq, PartialEq, Clone, Copy)]
+#[repr(usize)]
 pub enum EventType {
-    LocalData,
-    LocalOrder,
-    ExchData,
-    ExchOrder,
+    LocalData = 0,
+    LocalOrder = 1,
+    ExchData = 2,
+    ExchOrder = 3,
 }
 
+#[repr(C, align(64))]
+struct Align64([u8; 64]);
+
+fn aligned_vec_i64(count: usize) -> Box<[i64]> {
+    let capacity = (count * 8 / size_of::<Align64>()) + 1;
+    let mut aligned: Vec<Align64> = Vec::with_capacity(capacity);
+
+    let ptr = aligned.as_mut_ptr();
+    let cap = aligned.capacity();
+
+    forget(aligned);
+
+    unsafe {
+        Vec::from_raw_parts(ptr as *mut i64, count, cap * size_of::<Align64>() / 8).into_boxed_slice()
+    }
+}
+
+/// Manages the event timestamps to determine the next event to be processed.
 pub struct EventSet {
-    ev: Vec<Event>,
+    timestamp: Box<[i64]>,
     invalid: usize,
     num_assets: usize,
 }
 
 impl EventSet {
+    /// Constructs an instance of `EventSet`.
     pub fn new(num_assets: usize) -> Self {
         if num_assets == 0 {
             panic!();
         }
-        let mut ev = Vec::new();
-        for asset_no in 0..num_assets {
-            ev.push(Event {
-                timestamp: i64::MAX,
-                asset_no,
-                ty: EventType::LocalData,
-            });
-            ev.push(Event {
-                timestamp: i64::MAX,
-                asset_no,
-                ty: EventType::LocalOrder,
-            });
-            ev.push(Event {
-                timestamp: i64::MAX,
-                asset_no,
-                ty: EventType::ExchData,
-            });
-            ev.push(Event {
-                timestamp: i64::MAX,
-                asset_no,
-                ty: EventType::ExchOrder,
-            });
+        let mut timestamp = unsafe { aligned_vec_i64(num_assets * 4) };
+        for i in 0..(num_assets * 4) {
+            timestamp[i] = i64::MAX;
         }
         Self {
-            ev,
+            timestamp,
             invalid: 0,
             num_assets,
         }
     }
 
+    /// Returns the next event to be processed, which has the earliest timestamp.
     pub fn next(&self) -> Option<Event> {
         if self.invalid == self.num_assets {
             return None;
         }
-        let mut r = unsafe { *self.ev.get_unchecked(0) };
-        for ev in self.ev[1..].iter() {
-            if ev.timestamp < r.timestamp {
-                r = *ev;
+        let mut evst_no = 0;
+        let mut timestamp = unsafe { *self.timestamp.get_unchecked(0) };
+        for (i, &ev_timestamp) in self.timestamp[1..].iter().enumerate() {
+            if ev_timestamp < timestamp {
+                timestamp = ev_timestamp;
+                evst_no = i + 1;
             }
         }
-        Some(r)
+        let asset_no = evst_no >> 2;
+        let ty = unsafe { mem::transmute(evst_no & 3) };
+        Some(Event {
+            timestamp,
+            asset_no,
+            ty,
+        })
     }
 
+    #[inline]
     fn update(&mut self, evst_no: usize, timestamp: i64) {
-        let item = unsafe { self.ev.get_unchecked_mut(evst_no) };
-        item.timestamp = timestamp;
+        let item = unsafe { self.timestamp.get_unchecked_mut(evst_no) };
+        *item = timestamp;
     }
 
+    #[inline]
     pub fn update_local_data(&mut self, asset_no: usize, timestamp: i64) {
         self.update(4 * asset_no, timestamp);
     }
 
+    #[inline]
     pub fn update_local_order(&mut self, asset_no: usize, timestamp: i64) {
         self.update(4 * asset_no + 1, timestamp);
     }
 
+    #[inline]
     pub fn update_exch_data(&mut self, asset_no: usize, timestamp: i64) {
         self.update(4 * asset_no + 2, timestamp);
     }
 
+    #[inline]
     pub fn update_exch_order(&mut self, asset_no: usize, timestamp: i64) {
         self.update(4 * asset_no + 3, timestamp);
     }
 
+    #[inline]
     fn invalidate(&mut self, evst_no: usize) {
-        let item = unsafe { self.ev.get_unchecked_mut(evst_no) };
-        item.timestamp = i64::MAX;
+        let item = unsafe { self.timestamp.get_unchecked_mut(evst_no) };
+        *item = i64::MAX;
         self.invalid += 1;
     }
 
+    #[inline]
     pub fn invalidate_local_data(&mut self, asset_no: usize) {
         self.invalidate(4 * asset_no);
     }
 
+    #[inline]
     pub fn invalidate_exch_data(&mut self, asset_no: usize) {
         self.invalidate(4 * asset_no + 2);
     }
