7f86de08911b26b7cf55c7cfb14eaf5e84fe5996
nkaz001 <nkaz001@protonmail.com>
2024-06-25 11:21:15 -0400

    feat(rust): add `to_npz` to `BacktestRecorder`.

--- rust/src/backtest/recorder.rs
+++ rust/src/backtest/recorder.rs
@@ -4,15 +4,31 @@ use std::{
     path::Path,
 };
 
+use npyz::{npz::NpzWriter, AutoSerialize, WriterBuilder};
+use tokio::io::AsyncWriteExt;
+use zip_old::{write::FileOptions, CompressionMethod};
+
 use crate::{
     depth::MarketDepth,
     types::{Bot, BotTypedDepth, Recorder},
 };
 
+#[derive(AutoSerialize, npyz::Serialize)]
+struct Record {
+    timestamp: i64,
+    mid_price: f32,
+    balance: f64,
+    position: f64,
+    fee: f64,
+    trade_num: i32,
+    trade_amount: f64,
+    trade_qty: f64,
+}
+
 /// Provides recording of the backtesting strategy's state values, which are needed to compute
 /// performance metrics.
 pub struct BacktestRecorder {
-    values: Vec<Vec<(i64, f32, f64, f64, f64, i32, f64, f64)>>,
+    values: Vec<Vec<Record>>,
 }
 
 impl Recorder for BacktestRecorder {
@@ -29,16 +45,18 @@ impl Recorder for BacktestRecorder {
             let mid_price = (depth.best_bid() + depth.best_ask()) / 2.0;
             let state_values = hbt.state_values(asset_no);
             let values = unsafe { self.values.get_unchecked_mut(asset_no) };
-            values.push((
-                timestamp,
-                mid_price,
-                state_values.balance,
-                state_values.position,
-                state_values.fee,
-                state_values.trade_num,
-                state_values.trade_amount,
-                state_values.trade_qty,
-            ));
+            values.push(
+                (Record {
+                    timestamp,
+                    mid_price,
+                    balance: state_values.balance,
+                    position: state_values.position,
+                    fee: state_values.fee,
+                    trade_num: state_values.trade_num,
+                    trade_amount: state_values.trade_amount,
+                    trade_qty: state_values.trade_qty,
+                }),
+            );
         }
         Ok(())
     }
@@ -78,7 +96,7 @@ impl BacktestRecorder {
                 file,
                 "timestamp,mid_price,balance,position,fee,trade_num,trade_amount,trade_qty\n",
             )?;
-            for (
+            for Record {
                 timestamp,
                 mid_price,
                 balance,
@@ -87,7 +105,7 @@ impl BacktestRecorder {
                 trade_num,
                 trade_amount,
                 trade_qty,
-            ) in values
+            } in values
             {
                 write!(
                     file,
@@ -102,6 +120,29 @@ impl BacktestRecorder {
                     trade_qty
                 )?;
             }
+        }
+        Ok(())
+    }
+
+    pub fn to_npz<Prefix, P>(&self, prefix: Prefix, path: P) -> Result<(), Error>
+    where
+        Prefix: AsRef<str>,
+        P: AsRef<Path>,
+    {
+        let prefix = prefix.as_ref();
+        let file_path = path.as_ref().join(format!("{prefix}.npz"));
+        let mut npz = NpzWriter::create(file_path)?;
+        let options = FileOptions::default()
+            .compression_method(CompressionMethod::Bzip2)
+            .compression_level(Some(9));
+        for (asset_no, values) in self.values.iter().enumerate() {
+            let mut writer = npz
+                .array(&format!("{asset_no}"), options)?
+                .default_dtype()
+                .shape(&[values.len() as u64])
+                .begin_nd()?;
+            writer.extend(values)?;
+            writer.finish()?;
         }
         Ok(())
     }
