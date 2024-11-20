db2860627124ac2f2433bf2295c46347cf938a52
nkaz001 <nkaz001@protonmail.com>
2024-11-20 06:41:01 -0500

    chore(rust): update CI.

--- .github/workflows/install_deps.sh
+++ .github/workflows/install_deps.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-apt-get update
-apt-get install -y clang
+#apt-get update
+#apt-get install -y clang
+yum update -y
+yum install -y epel-release
+yum install -y clang
 version=clang --version
 echo $version
